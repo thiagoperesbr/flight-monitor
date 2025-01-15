0f986edf06ce6c1eaaf1ba711043a57cb06ab496
Thiago Mourão Araujo Peres <tperes@codin.gov>
2025-01-15 13:38:02 -0300

    Create Price and Layouver Filter

--- src/server.js
+++ src/server.js
@@ -120,64 +120,86 @@ async function sendTelegramMessage(message) {
   }
 }
 
-cron.schedule("* 4,8,12,16,20,24 * * *", async () => {
+const destinations = [
+  { departure_id: "GIG", arrival_id: "SSA", destino: "Salvador" },
+  { departure_id: "GIG", arrival_id: "REC", destino: "Recife" },
+  { departure_id: "GIG", arrival_id: "MCZ", destino: "Maceió" },
+]
+
+cron.schedule("* * * * *", async () => {
   console.log("Verificando passagens...");
 
-  const calendarPicker = await fetchCalendarPicker("GIG", "SSA");
-
-  const filteredFlights = calendarPicker.filter((picker) => picker.price < 700);
-
-  if (filteredFlights.length === 0) {
-    console.log("Nenhum voo de ida/volta abaixo de R$700 reais encontrado.");
-    return;
-  }
-
-  for (const filterflight of filteredFlights) {
-    const flights = await fetchSearchFlights(
-      "GIG",
-      "SSA",
-      filterflight.departure,
-      filterflight.return
-    );
-
-    const matchingFlights = [
-      ...(flights.itineraries?.topFlights || []),
-      ...(flights.itineraries?.otherFlights || []),
-    ].filter((flight) => flight.price === filterflight.price);
-
-    if (matchingFlights.length > 0) {
-      let consolidatedMessage =
-        `✈️ *Oferta de Passagem Aérea Encontrada!*\n\n` +
-        `- *Origem:* Rio de Janeiro (GIG)\n` +
-        `- *Destino:* Salvador (SSA)\n` +
-        `- *Data Ida:* ${formatDate(filterflight.departure)}\n` +
-        `- *Data Volta:* ${formatDate(filterflight.return)}\n` +
-        `- *Total (ida/volta):* R$ ${filterflight.price.toFixed(2)}\n\n`;
-
-      matchingFlights.forEach((flight, index) => {
-        let layoverInfo;
-
-        if (!flight.layovers) {
-          layoverInfo = "Direto";
-        } else if (flight.layovers.length === 1) {
-          layoverInfo = `1 parada (${flight.layovers[0].duration_label})`;
+  try {
+    for (const {departure_id, arrival_id, destino} of destinations) {
+      console.log(`Verificando voos de Rio de Janeiro (${departure_id}) para ${destino} (${arrival_id})`)
+  
+      const calendarPicker = await fetchCalendarPicker(departure_id, arrival_id);
+  
+      const filteredFlights = calendarPicker.filter((picker) => picker.price < 700);
+  
+      if (filteredFlights.length === 0) {
+        console.log(`Nenhum voo do Rio de Janeiro para ${destino} abaixo de R$ 700 reais encontrado.`);
+        continue;
+      }
+  
+      for (const filterflight of filteredFlights) {
+        const flights = await fetchSearchFlights(
+          departure_id,
+          arrival_id,
+          filterflight.departure,
+          filterflight.return
+        );
+  
+        const allFlights = [
+          ...(flights.itineraries?.topFlights || []),
+          ...(flights.itineraries?.otherFlights || [])
+        ]
+  
+        const matchingFlights = allFlights.filter((flight) => {
+          const isPriceMatch = flight.price === filterflight.price;
+          const isDirect = !flight.layovers;
+          const hasShortLayover = flight.layovers && flight.layovers.length === 1 && flight.layovers[0].duration <= 90;
+  
+          return isPriceMatch && (isDirect || hasShortLayover);
+        })
+  
+        if (matchingFlights.length > 0) {
+          let consolidatedMessage =
+            `✈️ *Oferta de Passagem Aérea Encontrada!*\n\n` +
+            `- *Origem:* Rio de Janeiro (${departure_id})\n` +
+            `- *Destino:* ${destino} (${arrival_id})\n` +
+            `- *Data Ida:* ${formatDate(filterflight.departure)}\n` +
+            `- *Data Volta:* ${formatDate(filterflight.return)}\n` +
+            `- *Total (ida/volta):* R$ ${filterflight.price.toFixed(2)}\n\n`;
+  
+          matchingFlights.forEach((flight, index) => {
+            let layoverInfo;
+  
+            if (!flight.layovers) {
+              layoverInfo = "Direto";
+            } else if (flight.layovers.length === 1) {
+              layoverInfo = `1 parada (${flight.layovers[0].duration_label})`;
+            } 
+  
+            consolidatedMessage +=
+              `*Voo ${index + 1}:*\n` +
+              `- *Companhia Aérea:* ${flight.flights[0].airline}\n` +
+              `- *Horário de Partida:* ${formatTime(flight.departure_time)}\n` +
+              `- *Horário de Chegada:* ${formatTime(flight.arrival_time)}\n` +
+              `- *Duração:* ${flight.duration?.text || "Indisponível"}\n` +
+              `- *Paradas:* ${layoverInfo}\n\n`;
+          });
+  
+          await sendTelegramMessage(consolidatedMessage);
         } else {
-          layoverInfo = `${flight.layovers.length} paradas`;
+          console.log(`Nenhum voo correspondente encontrado do Rio de Janeiro para ${destino}.`);
         }
-
-        consolidatedMessage +=
-          `*Voo ${index + 1}:*\n` +
-          `- *Companhia Aérea:* ${flight.flights[0].airline}\n` +
-          `- *Horário de Partida:* ${formatTime(flight.departure_time)}\n` +
-          `- *Horário de Chegada:* ${formatTime(flight.arrival_time)}\n` +
-          `- *Duração:* ${flight.duration?.text || "Indisponível"}\n` +
-          `- *Paradas:* ${layoverInfo}\n\n`;
-      });
-
-      await sendTelegramMessage(consolidatedMessage);
-    } else {
-      console.log("Nenhum voo correspondente encontrado.");
+      }
     }
+    
+    console.log("Concluída a verificação das passagens.")
+  } catch (err) {
+    console.error("Ocorreu um erro durante a verificação das passagens.")
   }
 });
 

1ef4273063aed5d86af378e3e892787ec22bbfed
Thiago Peres <thiago.mourao.peres@gmail.com>
2025-01-15 04:09:46 -0300

    Change Travel API & Improvements

--- src/server.js
+++ src/server.js
@@ -17,48 +17,95 @@ app.use(express.urlencoded({ extended: false }));
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
 
-function isMayDate(date) {
-  return date.startsWith("2025-05");
-}
-
-function addDaystoDate(date, days) {
-  const result = new Date(date);
-  result.setDate(result.getDate() + days);
-  return result.toISOString().split("T")[0];
-}
-
 function formatDate(dateString) {
   const [year, month, day] = dateString.split("-");
   return `${day}/${month}/${year}`;
 }
 
-async function fetchFlights(fromEntityId, toEntityId) {
+function formatTime(timeString) {
+  const [date, time, ampm] = timeString.split(" ", 3);
+
+  return `${time} ${ampm}`;
+}
+
+async function fetchCalendarPicker(departure_id, arrival_id) {
   try {
-    const departDate = new Date("2025-05-01").toISOString().split("T")[0];
+    const startDate = new Date("2025-05-01").toISOString().split("T")[0];
+    const endDate = new Date("2025-05-31").toISOString().split("T")[0];
 
     const options = {
       method: "GET",
-      url: "https://sky-scanner3.p.rapidapi.com/flights/price-calendar",
+      url: "https://google-flights2.p.rapidapi.com/api/v1/getCalendarPicker",
       params: {
-        fromEntityId,
-        toEntityId,
-        departDate,
-        market: "BR",
-        locale: "pt-BR",
+        departure_id,
+        arrival_id,
+        start_date: startDate,
+        end_date: endDate,
+        travel_class: "ECONOMY",
+        trip_type: "ROUND",
+        trip_days: "11",
+        adults: "1",
+        children: "0",
+        infant_on_lap: "0",
+        infant_in_seat: "0",
         currency: "BRL",
+        country_code: "BR",
       },
       headers: {
         "x-rapidapi-key": RAPIDAPI_KEY,
-        "x-rapidapi-host": "sky-scanner3.p.rapidapi.com",
+        "x-rapidapi-host": "google-flights2.p.rapidapi.com",
       },
     };
 
     const response = await axios.request(options);
 
-    return response.data.data.flights.days;
+    return response.data.data;
   } catch (err) {
     console.error(
-      `Erro ao buscar voos (${fromEntityId} - ${toEntityId}):`,
+      `Erro ao buscar voos (${departure_id} - ${arrival_id}):`,
+      err.message
+    );
+    return [];
+  }
+}
+
+async function fetchSearchFlights(
+  departure_id,
+  arrival_id,
+  outbound_date,
+  return_date
+) {
+  try {
+    const options = {
+      method: "GET",
+      url: "https://google-flights2.p.rapidapi.com/api/v1/searchFlights",
+      params: {
+        departure_id,
+        arrival_id,
+        outbound_date,
+        return_date,
+        travel_class: "ECONOMY",
+        adults: "1",
+        children: "0",
+        infant_on_lap: "0",
+        infant_in_seat: "0",
+        show_hidden: "1",
+        currency: "BRL",
+        language_code: "pt-BR",
+        country_code: "BR",
+      },
+      headers: {
+        "x-rapidapi-key": RAPIDAPI_KEY,
+        "x-rapidapi-host": "google-flights2.p.rapidapi.com",
+      },
+    };
+
+    const response = await axios.request(options);
+
+    return response.data.data;
+  } catch (err) {
+    console.error(
+      `Erro ao buscar voos (${departure_id} - ${arrival_id}):`,
       err.message
     );
     return [];
@@ -73,62 +120,64 @@ async function sendTelegramMessage(message) {
   }
 }
 
-cron.schedule("0 8,12,16,20 * * *", async () => {
+cron.schedule("* 4,8,12,16,20,24 * * *", async () => {
   console.log("Verificando passagens...");
 
-  const outboundFlights = await fetchFlights("GIG", "SSA");
+  const calendarPicker = await fetchCalendarPicker("GIG", "SSA");
 
-  const mayOutboundFlights = outboundFlights.filter(
-    (flight) => isMayDate(flight.day) && flight.price < 360.0
-  );
+  const filteredFlights = calendarPicker.filter((picker) => picker.price < 700);
 
-  if (mayOutboundFlights.length === 0) {
-    console.log("Nenhum voo de ida abaixo de R$350 encontrado.");
+  if (filteredFlights.length === 0) {
+    console.log("Nenhum voo de ida/volta abaixo de R$700 reais encontrado.");
     return;
   }
 
-  const returnDates = mayOutboundFlights.map((flight) => ({
-    outbound: flight,
-    returnDate: addDaystoDate(flight.day, 12),
-  }));
-
-  const inboundFlights = await fetchFlights("SSA", "GIG");
-
-  const matchedFlights = [];
-
-  for (const { outbound, returnDate } of returnDates) {
-    const matchingReturnFlight = inboundFlights.find(
-      (flight) => flight.day === returnDate && flight.price < 360.0
+  for (const filterflight of filteredFlights) {
+    const flights = await fetchSearchFlights(
+      "GIG",
+      "SSA",
+      filterflight.departure,
+      filterflight.return
     );
 
-    if (matchingReturnFlight) {
-      matchedFlights.push({
-        outbound,
-        inbound: matchingReturnFlight,
-      });
-    }
-  }
+    const matchingFlights = [
+      ...(flights.itineraries?.topFlights || []),
+      ...(flights.itineraries?.otherFlights || []),
+    ].filter((flight) => flight.price === filterflight.price);
 
-  if (matchedFlights.length > 0) {
-    const messages = matchedFlights.map(({ outbound, inbound }) => {
-      const totalPrice = (outbound.price + inbound.price).toFixed(2);
-      return (
+    if (matchingFlights.length > 0) {
+      let consolidatedMessage =
         `✈️ *Oferta de Passagem Aérea Encontrada!*\n\n` +
         `- *Origem:* Rio de Janeiro (GIG)\n` +
         `- *Destino:* Salvador (SSA)\n` +
-        `- *Data Ida:* ${formatDate(outbound.day)}\n` +
-        `- *Preço Ida:* R$ ${outbound.price.toFixed(2)}\n\n` +
-        `- *Origem:* Salvador (SSA)\n` +
-        `- *Destino:* Rio de Janeiro (SDU)\n` +
-        `- *Data Volta:* ${formatDate(inbound.day)}\n` +
-        `- *Preço Volta:* R$ ${inbound.price.toFixed(2)}\n\n` +
-        `- *Total (ida/volta):* R$ ${totalPrice}\n`
-      );
-    });
+        `- *Data Ida:* ${formatDate(filterflight.departure)}\n` +
+        `- *Data Volta:* ${formatDate(filterflight.return)}\n` +
+        `- *Total (ida/volta):* R$ ${filterflight.price.toFixed(2)}\n\n`;
 
-    await sendTelegramMessage(messages.join("\n\n"));
-  } else {
-    console.log("Nenhuma combinação de ida e volta encontrada.");
+      matchingFlights.forEach((flight, index) => {
+        let layoverInfo;
+
+        if (!flight.layovers) {
+          layoverInfo = "Direto";
+        } else if (flight.layovers.length === 1) {
+          layoverInfo = `1 parada (${flight.layovers[0].duration_label})`;
+        } else {
+          layoverInfo = `${flight.layovers.length} paradas`;
+        }
+
+        consolidatedMessage +=
+          `*Voo ${index + 1}:*\n` +
+          `- *Companhia Aérea:* ${flight.flights[0].airline}\n` +
+          `- *Horário de Partida:* ${formatTime(flight.departure_time)}\n` +
+          `- *Horário de Chegada:* ${formatTime(flight.arrival_time)}\n` +
+          `- *Duração:* ${flight.duration?.text || "Indisponível"}\n` +
+          `- *Paradas:* ${layoverInfo}\n\n`;
+      });
+
+      await sendTelegramMessage(consolidatedMessage);
+    } else {
+      console.log("Nenhum voo correspondente encontrado.");
+    }
   }
 });
 
